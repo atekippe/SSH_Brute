10b2a96382441340c188dc67a96ed58c532b0f38
Aaron Tekippe <atekippe@hotmail.com>
2018-09-02 21:13:53 -0500

    Added debug and instructions.

--- pin_shell.py
+++ pin_shell.py
@@ -1,12 +1,16 @@
-# Script that ask for a pin for testing.
+# Script prompts for a pin.  Used to test SSH_Brute_Pin.py
+# set in the /etc/ssh/sshd_config to run -> ForceCommand /home/cdc/pin.py
+
+#!/usr/bin/python3
+
+f = open('/tmp/pin_debug', 'a')
 
 while 1 is 1:
+    p = 9999
     pin = int(input("Pin: "))
-
-    if pin is 12:
+    f.write(str(pin) + "\n")
+    if pin == p:
         print("Correct")
         exit()
     else:
         print("Incorrect")
-
-
